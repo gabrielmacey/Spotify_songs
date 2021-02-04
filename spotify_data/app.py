9e09ed3e1748dce5b2805bb9d4bce551232d109c
gabrielmacey <gabem0928@yahoo.com>
2021-02-03 21:38:19 -0500

    App.py

--- spotify_data/app.py
+++ spotify_data/app.py
@@ -1,9 +1,10 @@
 #Import Flask
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import json
 from bson import json_util
 from bson.json_util import dumps
+
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
@@ -18,11 +19,39 @@ mongo = PyMongo(app)
 def index():
     spot_songs = mongo.db.songs.find_one()
     artists = []
+    release = []
+    hard = []
+    soft = []
+    art = "Sam Hunt"
+    release_year = "all"
     for object in mongo.db.songs.find({}, {"_id":False}):
-        artists.append(object["artist"])
-    return render_template("index.html", artists=artists)
+        artist = object["artist"]
+        if artist not in artists:
+            artists.append(artist)
+        else:
+            exit
+        sep = object["album_release_date"].split("-")
+        year = sep[0]
+        if year not in release:
+            release.append(year)
+        else:
+            exit
+        if (artist == art) and (release_year == "all"):
+            hard.append(object)
+        elif (artist == art) and (year == release_year):
+            hard.append(object)
+        else:
+            exit
+    return render_template("index.html", artists=artists, release=release, hard=hard)
 
-
+@app.route('/data/<artist>')
+def data(artist):
+    results = mongo.db.songs.find({"artist":artist}, {"_id":False})
+    # results = mongo.db.songs.find({"artist":artist}, {"_id":False})
+    data1 = []
+    for result in results:
+        data1.append(result)
+    return jsonify(data1)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- spotify_data/static/js/logic.js
+++ spotify_data/static/js/logic.js
@@ -1,7 +1,31 @@
-console.log(artists);
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
 
-// Append to make id strings
-d3.json(query).then((data)=>{
-  var string = data.Artists;
-  console.log(string);
-});
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
+d3.json("/artist/data/").then(function() {
+  // EventHandler
+
+}
